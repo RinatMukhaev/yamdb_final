26d1d31b37176d0923484e12f3aad30c94f175ea
RinatMukhaev <rinatmuhaev@yandex.ru>
2022-10-18 01:05:29 +0300

    flake

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,7 +3,6 @@ import datetime as dt
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
